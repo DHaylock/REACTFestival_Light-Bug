ebc295862ff0a746a85ce1f858a4f19561ec275d
David Haylock <david.haylock@watershed.co.uk>
2015-10-08 23:43:51 +0100

    - added .app file to gitignore - Created Color list

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,61 +1,51 @@
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void ofApp::setup(){
+void ofApp::setupColors()
+{
+    colorsArray.push_back(RED_1);
+    colorsArray.push_back(RED_2);
+    colorsArray.push_back(RED_3);
+    colorsArray.push_back(RED_4);
+    
+    colorsArray.push_back(GREEN_1);
+    colorsArray.push_back(GREEN_2);
+    colorsArray.push_back(GREEN_3);
+    colorsArray.push_back(GREEN_4);
+    
+    colorsArray.push_back(BLUE_1);
+    colorsArray.push_back(BLUE_2);
+    colorsArray.push_back(BLUE_3);
+    colorsArray.push_back(BLUE_4);
+    
+    colorsArray.push_back(WHITE);
+    colorsArray.push_back(OFF);
+}
+//--------------------------------------------------------------
+void ofApp::setup()
+{
+    setupColors();
+}
+//--------------------------------------------------------------
+void ofApp::update()
+{
 
 }
-
 //--------------------------------------------------------------
-void ofApp::update(){
+void ofApp::draw()
+{
+    for(int i = 0; i < colorsArray.size(); i++) {
+        ofSetColor(colorsArray[i]);
+        ofCircle((ofGetWidth()/2-(colorsArray.size()/2*25))+(i*25),ofGetHeight()/2,10);
+    }
+}
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key)
+{
 
 }
-
 //--------------------------------------------------------------
-void ofApp::draw(){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::keyPressed(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::keyReleased(int key)
+{
 
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -2,6 +2,29 @@
 
 #include "ofMain.h"
 
+//------------------------------------------------
+// *
+// * Color Definitions
+// *
+//------------------------------------------------
+#define RED_1 ofColor(255,80,80)
+#define RED_2 ofColor(255,0,0)
+#define RED_3 ofColor(255,50,0)
+#define RED_4 ofColor(255,80,0)
+
+#define GREEN_1 ofColor(255,255,0)
+#define GREEN_2 ofColor(0,255,0)
+#define GREEN_3 ofColor(0,220,10)
+#define GREEN_4 ofColor(0,140,40)
+
+#define BLUE_1 ofColor(0,220,170)
+#define BLUE_2 ofColor(0,0,255)
+#define BLUE_3 ofColor(40,0,170)
+#define BLUE_4 ofColor(255,0,255)
+
+#define WHITE ofColor(255,255,255)
+#define OFF ofColor(0,0,0)
+
 class ofApp : public ofBaseApp{
 
 	public:
@@ -11,12 +34,9 @@ class ofApp : public ofBaseApp{
 
 		void keyPressed(int key);
 		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
+    
+    
+        // Make the colors
+        void setupColors();
+        vector <ofColor> colorsArray;
 };
