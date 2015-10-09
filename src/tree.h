fda3a7cad3dbd1be4973c881bb7bf1a2253164c4
David Haylock <david.haylock@watershed.co.uk>
2015-10-09 12:10:43 +0100

    - Tree implemented

--- src/tree.h
+++ src/tree.h
@@ -6,8 +6,53 @@
 //
 //
 
-#ifndef tree_h
-#define tree_h
+#include "ofMain.h"
 
-
-#endif /* tree_h */
+//--------------------------------------------------------------
+class Tree {
+    
+    public:
+        //--------------------------------------------------------------
+        void setup(int id,int addressStart, int numChannels = 3)
+        {
+            _id = id;
+            _address = addressStart;
+            cout << "--------------------" << endl;
+            cout << "Tree" << endl;
+            cout << "ID: " << id << endl;
+            cout << "Start Channel: " <<  addressStart << endl;
+        }
+        //--------------------------------------------------------------
+        void update()
+        {
+            
+        }
+        //--------------------------------------------------------------
+        void draw(int x, int y)
+        {
+            ofSetColor(_C);
+            ofCircle(x, y, 20);
+        }
+        //--------------------------------------------------------------
+        void setColor(ofColor c)
+        {
+            _C = c;
+        }
+        //--------------------------------------------------------------
+        ofColor getColor()
+        {
+            return _C;
+        }
+        //--------------------------------------------------------------
+        int getAddresses(){
+            return _address;
+        }
+    
+    private:
+        
+    protected:
+        ofColor _C;
+    
+        int _id;
+        int _address;
+};
